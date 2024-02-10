0731630c2b955c294cab2ff1004304c7ff0b49c0
Mireia <mireia.ma.ar@gmail.com>
2024-02-10 19:58:38 +0100

    create calculator module

---
+++ calculator.js
@@ -0,0 +1,13 @@
+let suma=require("./sum.js")
+let resta=require("./subs.js")
+let multi=require("./multi.js")
+let div=require("./div.js")
+let modulo=require("./mod.js")
+
+module.exports={
+    suma,
+    resta,
+    multi,
+    div,
+    modulo
+}
