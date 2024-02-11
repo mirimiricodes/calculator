ebaf986f8e4e05a16c6a28137f979460a5c1f5ce
Mireia <mireia.ma.ar@gmail.com>
2024-02-11 21:30:22 +0100

    add description to tests

--- test/test.js
+++ test/test.js
@@ -16,24 +16,25 @@ function print(operacion, resultado) {
 // print(`de la división`, calc.div(n1,n2))
 // print(`del resto`, calc.modulo(n1,n2))
 
-function assert(input, expected) {
+function assertEquals(description, input, expected) {
+
     if (input==expected) {
-        console.log("OK!")
+        console.log(`${description}: OK!`)
     }
-    if (input!=expected) {
-        console.log("ERROR")
+    else {
+        console.log(`${description}: ERROR!`)
     }
 }
-assert(calc.suma(1,1), 2)
-assert(calc.suma(30,4), 34)
-assert(calc.resta(6,2), 4)
-assert(calc.resta(10,6), 4)
-assert(calc.multi(4,6), 24)
-assert(calc.multi(5,6), 30)
-assert(calc.div(35,5), 7)
-assert(calc.div(21,7), 3)
-assert(calc.modulo(2,2), 0)
-assert(calc.modulo(7,2), 1)
+assertEquals("Suma 1 y 1", calc.suma(1,1), 2)
+assertEquals("Suma 30 y 4", calc.suma(30,4), 34) 
+assertEquals("Resta 6 y 2", calc.resta(6,2), 4)
+assertEquals("Resta 10 y 6", calc.resta(10,6), 4)
+assertEquals("Multiplicación 4 y 6", calc.multi(4,6), 24)
+assertEquals("Multiplicación 5 y 6", calc.multi(5,6), 30)
+assertEquals("División 35 y 7", calc.div(35,5), 7)
+assertEquals("División 21 y 7", calc.div(21,7), 3)
+assertEquals("Resto 2 y 2", calc.modulo(2,2), 0)
+assertEquals("Resto 7 y 2", calc.modulo(7,2), 1)
 
 
 
