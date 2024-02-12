e9ddcde99cf3c56b6f33233549dc16efe19b9c6b
Mireia <mireia.ma.ar@gmail.com>
2024-02-12 20:42:25 +0100

    error handling in sum

--- src/sum.js
+++ src/sum.js
@@ -1,6 +1,17 @@
-function sum(n1,n2) {
-	return n1+n2
+function sum(n1, n2) {
+	//IF n1 or n2 is not a number, show error message, else return n1+n2
+	let typeOfN1 = typeof n1
+	let n1IsANumber = typeOfN1 == "number"
+	let typeOfN2 = typeof n2
+	let n2IsANumber = typeOfN2 == "number"
+
+	if (n1IsANumber == true && n2IsANumber == true) {
+		return n1 + n2
+	}
+	else {
+		return "not valid number"
+	}
 }
-module.exports=sum
+module.exports = sum
 
 
