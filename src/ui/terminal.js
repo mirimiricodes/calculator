4462e319c5d2f52ceea2d157920c1673e38fae9f
Mireia <mireia.ma.ar@gmail.com>
2024-02-14 19:49:28 +0100

    improve ui

--- src/ui/terminal.js
+++ src/ui/terminal.js
@@ -3,40 +3,70 @@ let readline = require("readline-sync")
 
 function render() {
     renderMenu()
-    let { userSelection, userN1, userN2 } = collectUserData()
-    showResult(userSelection, userN1, userN2)
+    let userSelection = readline.question()
+    showChosenOperation(userSelection)
+    let { userN1, userN2 } = chooseNumbers()
+    let result = computeResult(userSelection, userN1, userN2)
+    showResult(result)
     render()
 }
 
-function showResult(userSelection, userN1, userN2) {
+function showResult(result) {
+    console.log(result)
+}
+
+function computeResult(userSelection, userN1, userN2) {
     switch (userSelection) {
         case "1":
-            console.log(calc.suma(userN1, userN2))
-            break
+            return calc.suma(userN1, userN2)
         case "2":
-            console.log(calc.resta(userN1, userN2))
-            break
-        default:
-            console.log("Choose an operation (1 or 2)")
-            break
+            return calc.resta(userN1, userN2)
+        case "3":
+            return calc.multi(userN1, userN2)
+        case "4":
+            return calc.div(userN1, userN2)
     }
 }
 
-function collectUserData() {
-    let userSelection = readline.question()
-    console.log("Choose 1st number:")
-    let userN1 = readline.questionInt()
-    console.log("Choose 2nd number:")
-    let userN2 = readline.questionInt()
-    return { userSelection, userN1, userN2 }
+function showChosenOperation(userSelection) {
+    switch (userSelection) {
+        case "1":
+            console.log("Great! You chose an addition!")
+            break
+        case "2":
+            console.log("Let's substract!")
+            break
+        case "3":
+            console.log("I see you want to multiply...")
+            break
+        case "4":
+            console.log("Looks like you want to divide...")
+            break
+        default:
+            "Choose an operation (1, 2, 3 or 4)"
+    }
 }
+
+
+function chooseNumbers() {
+    console.log("# Choose 1st number:")
+    let userN1 = readline.questionInt()
+    console.log("# Choose 2nd number:")
+    let userN2 = readline.questionInt()
+    return { userN1, userN2 }
+}
+
+
 
 function renderMenu() {
-    console.log("Choose an operation:")
-    console.log("1. SUM")
+    console.log("# Choose an operation:")
+    console.log("1. ADDITION")
     console.log("2. SUBSTRACTION")
+    console.log("3. MULTIPLICATION")
+    console.log("4. DIVISION")
+    console.log("")
 }
 
-console.log("Calculator")
+console.log("Welcome to Mirimiri's first calculator!")
 console.log("")
 render()
